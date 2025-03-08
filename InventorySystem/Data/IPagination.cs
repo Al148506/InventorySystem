d8f26929ef0833f490a8a4204e861c597da9bafb
Al148506 <al148506@gmail.com>
2025-03-07 18:49:00 -0600

    Pagination partial view for reutilizatio

---
+++ InventorySystem/Data/IPagination.cs
@@ -0,0 +1,8 @@
+﻿namespace InventorySystem.Data
+{
+    public interface IPagination
+    {
+        int InitialPage { get; }
+        int TotalPages { get; }
+    }
+}
---
+++ InventorySystem/Models/ViewModels/PaginationViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InventorySystem.Models.ViewModels
+{
+    public class PaginationViewModel
+    {
+        public int InitialPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
